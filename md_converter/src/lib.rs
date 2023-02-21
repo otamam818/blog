79bb81f0985e14e9b1f6f6ab13e66ca608a734d1
otamam818 <otamam818@gmail.com>
2023-02-22 08:01:27 +1100

    Implemented parsers for H1s and UnorderedLists

--- md_converter/src/lib.rs
+++ md_converter/src/lib.rs
@@ -1,33 +1,53 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
+const INDENT_SPACES: usize = 2;
 
-#[wasm_bindgen]
+type Indentations = usize;
+
 #[derive(PartialEq, Debug)]
 pub enum MarkdownForm {
     Heading,
-    UnorderedList,
-    OrderedList,
+    /// UnorderedLists and OrderedLists contains a set number of spaces to
+    /// identify its indentation
+    UnorderedList(Indentations),
+    OrderedList(Indentations),
     Code,
     PrewrittenHTML,
     PlainText
 }
 
+#[derive(PartialEq, Debug)]
 pub struct MarkdownData {
     pub form: MarkdownForm,
     pub inner_data: String
 }
 
 #[wasm_bindgen]
-pub fn get_form(line: &str) -> MarkdownForm {
+pub fn convert_text(data: String) -> String {
+    // Make it a Vec<MarkdownData> data
+    // Parse the Vec<MarkdownData> data into a handlebars file
+    format!("TODO: Parse the following\n{}", data)
+}
+
+pub fn get_form(line: &str) -> MarkdownData {
+    // Default values
+    let line = line.to_string();
+    let mut form = MarkdownForm::PlainText;
+    let mut inner_data = line.clone();
+
+    // Values to be re-assigned if it matches any other condition
     if line.starts_with("#") {
-        return MarkdownForm::Heading;
+        form = MarkdownForm::Heading;
+        inner_data = line.trim_matches('#').trim().to_string();
+    } else if line.trim().starts_with("-") {
+        let indents = ( line.len() - line.trim().len() ) / INDENT_SPACES;
+        form = MarkdownForm::UnorderedList(indents);
+
+        inner_data = line.trim().trim_matches('-').trim().to_string();
     }
 
-    MarkdownForm::PlainText
+    let inner_data = inner_data.to_owned();
+    MarkdownData { form, inner_data }
 }
 
 #[cfg(test)]
@@ -37,6 +57,27 @@ mod tests {
     #[test]
     fn headings_understood() {
         let line = String::from("# Hello world");
-        assert_eq!(get_form(&line), MarkdownForm::Heading);
+        let data = MarkdownData {
+            form: MarkdownForm::Heading,
+            inner_data: "Hello world".to_string()
+        };
+        assert_eq!(get_form(&line), data);
+    }
+
+    #[test]
+    fn unordered_list_understood() {
+        let line = String::from("- Hello world");
+        let data = MarkdownData {
+            form: MarkdownForm::UnorderedList(0),
+            inner_data: "Hello world".to_string()
+        };
+        assert_eq!(get_form(&line), data);
+
+        let line = String::from("  - Hello world");
+        let data = MarkdownData {
+            form: MarkdownForm::UnorderedList(1),
+            inner_data: "Hello world".to_string()
+        };
+        assert_eq!(get_form(&line), data);
     }
 }
