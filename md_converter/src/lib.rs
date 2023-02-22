f0dc844ba40911f56d66f263255d011a8ef9210f
otamam818 <otamam818@gmail.com>
2023-02-22 18:13:52 +1100

    implemented the main logic of the 'convert_text' wasm function, made a function to get the provided struct 'MarkdownData' as a vector of structs to exhaustively handle all markdown cases, implemented extensive version of headings to support beyond h1 elements, updated tests

--- md_converter/src/lib.rs
+++ md_converter/src/lib.rs
@@ -1,14 +1,22 @@
+use std::collections::HashSet;
+
 use wasm_bindgen::prelude::*;
 
+/// Depending on the method of parsing, the number-of-spaces per indent is
+/// often different. By default, like on GitHub, it is 2 spaces
 const INDENT_SPACES: usize = 2;
 
+/// A heading can be an h1, h2, etc, we need to know the number to render
+/// it appropriately
+type HeadingNumber = usize;
+
+/// UnorderedLists and OrderedLists contains a set number of spaces to
+/// identify its indentation
 type Indentations = usize;
 
 #[derive(PartialEq, Debug)]
 pub enum MarkdownForm {
-    Heading,
-    /// UnorderedLists and OrderedLists contains a set number of spaces to
-    /// identify its indentation
+    Heading(HeadingNumber),
     UnorderedList(Indentations),
     OrderedList(Indentations),
     Code,
@@ -24,12 +32,55 @@ pub struct MarkdownData {
 
 #[wasm_bindgen]
 pub fn convert_text(data: String) -> String {
-    // Make it a Vec<MarkdownData> data
-    // Parse the Vec<MarkdownData> data into a handlebars file
-    format!("TODO: Parse the following\n{}", data)
+    let md_vec = get_md_vec(&data);
+    md_vec
+        .iter()
+        .map(|md_data| parse_md_data(md_data))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
 
-pub fn get_form(line: &str) -> MarkdownData {
+/// Parses the MarkdownData into a handlebars string
+pub fn parse_md_data(md_data: &MarkdownData) -> String {
+    todo!()
+}
+
+pub fn get_md_vec(data: &str) -> Vec<MarkdownData> {
+    use MarkdownForm::*;
+    let non_plains = HashSet::from(['#', '-', '`']);
+    let mut fin_vec = Vec::new();
+    for line in data.lines() {
+        let first_char = line.trim_start().chars().nth(0).unwrap();
+        if non_plains.contains(&first_char) {
+            fin_vec.push(parse_line(line));
+            continue;
+        }
+
+        match fin_vec.last() {
+            Some(md_atom) => {
+                match md_atom.form {
+                    PlainText => {
+                        let prev = fin_vec.pop().unwrap();
+                        fin_vec.push(MarkdownData {
+                            form: PlainText,
+                            inner_data: format!(
+                                "{}\n{}",
+                                prev.inner_data,
+                                line
+                            )
+                        });
+                    },
+                    _ => fin_vec.push(parse_line(line))
+                }
+            },
+            None => fin_vec.push(parse_line(line))
+        };
+    }
+
+    fin_vec
+}
+
+pub fn parse_line(line: &str) -> MarkdownData {
     // Default values
     let line = line.to_string();
     let mut form = MarkdownForm::PlainText;
@@ -37,13 +88,19 @@ pub fn get_form(line: &str) -> MarkdownData {
 
     // Values to be re-assigned if it matches any other condition
     if line.starts_with("#") {
-        form = MarkdownForm::Heading;
-        inner_data = line.trim_matches('#').trim().to_string();
+        let line_without_hash = line.trim_matches('#');
+        let heading_number = line.len() - line_without_hash.len();
+        assert!(heading_number <= 6);
+
+        form = MarkdownForm::Heading(heading_number);
+        inner_data = line_without_hash.trim().to_string();
     } else if line.trim().starts_with("-") {
         let indents = ( line.len() - line.trim().len() ) / INDENT_SPACES;
-        form = MarkdownForm::UnorderedList(indents);
 
-        inner_data = line.trim().trim_matches('-').trim().to_string();
+        form = MarkdownForm::UnorderedList(indents);
+        inner_data = line
+            .trim().trim_matches('-').trim()
+            .to_string();
     }
 
     let inner_data = inner_data.to_owned();
@@ -58,10 +115,17 @@ mod tests {
     fn headings_understood() {
         let line = String::from("# Hello world");
         let data = MarkdownData {
-            form: MarkdownForm::Heading,
+            form: MarkdownForm::Heading(1),
             inner_data: "Hello world".to_string()
         };
-        assert_eq!(get_form(&line), data);
+        assert_eq!(parse_line(&line), data);
+
+        let line = String::from("## Secondary title");
+        let data = MarkdownData {
+            form: MarkdownForm::Heading(2),
+            inner_data: "Secondary title".to_string()
+        };
+        assert_eq!(parse_line(&line), data);
     }
 
     #[test]
@@ -71,13 +135,13 @@ mod tests {
             form: MarkdownForm::UnorderedList(0),
             inner_data: "Hello world".to_string()
         };
-        assert_eq!(get_form(&line), data);
+        assert_eq!(parse_line(&line), data);
 
         let line = String::from("  - Hello world");
         let data = MarkdownData {
             form: MarkdownForm::UnorderedList(1),
             inner_data: "Hello world".to_string()
         };
-        assert_eq!(get_form(&line), data);
+        assert_eq!(parse_line(&line), data);
     }
 }
