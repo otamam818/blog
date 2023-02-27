427f15bc296bcab08b5bfc5b5a15d7fdbbff7e81
otamam818 <otamam818@gmail.com>
2023-02-27 12:45:50 +1100

    Started working on new implementation of getting the markdown vector, updated tests

---
+++ md_converter/src/parser/flattener.rs
@@ -0,0 +1,68 @@
+use crate::data_models::{MarkdownForm, MarkdownData};
+
+// TODO: Use this code eventually in the `get_md_vec` function
+#[allow(dead_code)]
+pub fn flatten_md_data(init_vec: Vec<MarkdownData>) -> Vec<MarkdownData> {
+    let mut fin_vec = Vec::new();
+    let mut vec_iter = init_vec.iter();
+    let mut md_next = vec_iter.next();
+    while let Some(mut md_atom) = md_next {
+        let mut data_holder = Vec::new();
+        while let MarkdownForm::PlainText { has_line_break } = md_atom.form {
+            data_holder.push(
+                MarkdownData {
+                    form: MarkdownForm::PlainText { has_line_break },
+                    inner_data: md_atom.inner_data.clone()
+                }
+            );
+
+            md_next = vec_iter.next();
+            match md_next {
+                Some(atom) => md_atom = atom,
+                None => break
+            };
+        }
+
+        if data_holder.len() > 0 {
+            let mut curr_md_form = data_holder.pop().expect("len > 0");
+            while data_holder.len() > 0 {
+                let new_data = data_holder.pop().expect("len > 0");
+                curr_md_form = MarkdownData {
+                    form: curr_md_form.form,
+                    inner_data: format!(
+                        "{} {}",
+                        new_data.inner_data,
+                        curr_md_form.inner_data
+                    )
+                }
+            }
+            fin_vec.push(curr_md_form);
+        }
+    }
+
+    fin_vec
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn flattens_plaintext() {
+        let data = vec![
+            MarkdownData {
+                form: MarkdownForm::PlainText { has_line_break: true },
+                inner_data: "Line 1".to_string()
+            },
+            MarkdownData {
+                form: MarkdownForm::PlainText { has_line_break: true },
+                inner_data: "Still line 1".to_string()
+            }
+        ];
+        let data = flatten_md_data(data);
+        let data = data.get(0).expect("Data made manually");
+        assert_eq!(data.inner_data, "Line 1 Still line 1".to_string());
+        assert_eq!(data.form, MarkdownForm::PlainText { has_line_break: true });
+    }
+}
--- md_converter/src/parser/mod.rs
+++ md_converter/src/parser/mod.rs
@@ -1,5 +1,6 @@
 mod parsers;
 mod checkers;
+mod flattener;
 
 use crate::{
     data_models::{MarkdownData, MarkdownForm},
@@ -7,8 +8,13 @@ use crate::{
 };
 
 pub fn get_md_vec(data: &str) -> Vec<MarkdownData> {
+    let fin_vec = data.lines()
+        .map(|line| parse_line(line))
+        .collect::<Vec<MarkdownData>>();
+
+    /*
     let md_chars = SyntaxChecker::md_chars();
-    let mut fin_vec = Vec::new();
+    let mut fin_vec: Vec<MarkdownData> = Vec::new();
 
     for line in data.lines() {
         let first_char = line.trim_start().chars().nth(0).unwrap();
@@ -20,7 +26,7 @@ pub fn get_md_vec(data: &str) -> Vec<MarkdownData> {
         match fin_vec.last() {
             Some(md_atom) => {
                 match md_atom.form {
-                    MarkdownForm::PlainText
+                    MarkdownForm::PlainText { .. }
                         => modify_plaintext(&mut fin_vec, line),
 
                     _ => fin_vec.push(parse_line(line))
@@ -29,16 +35,19 @@ pub fn get_md_vec(data: &str) -> Vec<MarkdownData> {
             None => fin_vec.push(parse_line(line))
         };
     }
+    */
+
+    // TODO: Flatten all adjacent MarkdownForm Lists
 
     fin_vec
 }
 
-fn parse_line(line: &str) -> MarkdownData {
+pub fn parse_line(line: &str) -> MarkdownData {
     let numbers = SyntaxChecker::numbers();
 
     // Default values
     let line = line.to_string();
-    let mut form = MarkdownForm::PlainText;
+    let mut form = MarkdownForm::new_plaintext();
     let mut inner_data = line.clone();
 
     // Values to be re-assigned if it matches any other condition
@@ -55,18 +64,6 @@ fn parse_line(line: &str) -> MarkdownData {
     MarkdownData { form, inner_data }
 }
 
-fn modify_plaintext (fin_vec: &mut Vec<MarkdownData>, line: &str) {
-    let prev = fin_vec.pop().unwrap();
-    fin_vec.push(MarkdownData {
-        form: MarkdownForm::PlainText,
-        inner_data: format!(
-            "{}\n{}",
-            prev.inner_data,
-            line
-        )
-    });
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,14 +72,14 @@ mod tests {
     fn headings_understood() {
         let line = String::from("# Hello world");
         let data = MarkdownData {
-            form: MarkdownForm::Heading(1),
+            form: MarkdownForm::Heading { heading_number: 1 },
             inner_data: "Hello world".to_string()
         };
         assert_eq!(parse_line(&line), data);
 
         let line = String::from("## Secondary title");
         let data = MarkdownData {
-            form: MarkdownForm::Heading(2),
+            form: MarkdownForm::Heading { heading_number: 2 },
             inner_data: "Secondary title".to_string()
         };
         assert_eq!(parse_line(&line), data);
@@ -92,14 +89,14 @@ mod tests {
     fn unordered_list_understood() {
         let line = String::from("- Hello world");
         let data = MarkdownData {
-            form: MarkdownForm::UnorderedList(0),
+            form: MarkdownForm::UnorderedList { indents: 0, inner_bullet: None },
             inner_data: "Hello world".to_string()
         };
         assert_eq!(parse_line(&line), data);
 
         let line = String::from("  - Hello world");
         let data = MarkdownData {
-            form: MarkdownForm::UnorderedList(1),
+            form: MarkdownForm::UnorderedList { indents: 1, inner_bullet: None },
             inner_data: "Hello world".to_string()
         };
         assert_eq!(parse_line(&line), data);
@@ -109,14 +106,22 @@ mod tests {
     fn ordered_list_understood() {
         let line = String::from("1. Hello world");
         let data = MarkdownData {
-            form: MarkdownForm::OrderedList((0, 1)),
+            form: MarkdownForm::OrderedList {
+                indents: 0,
+                current_number: 1,
+                inner_bullet: None
+            },
             inner_data: "Hello world".to_string()
         };
         assert_eq!(parse_line(&line), data);
 
         let line = String::from("  2. Hello world");
         let data = MarkdownData {
-            form: MarkdownForm::OrderedList((1, 2)),
+            form: MarkdownForm::OrderedList {
+                indents: 1,
+                current_number: 2,
+                inner_bullet: None
+            },
             inner_data: "Hello world".to_string()
         };
         assert_eq!(parse_line(&line), data);
