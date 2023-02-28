4bbdd20a79453185739d6b1d3f7682a7278a725c
otamam818 <otamam818@gmail.com>
2023-02-28 14:47:35 +1100

    Added function to flatten adjacent unordered lists into a single enum in the final vector

--- md_converter/src/parser/flattener.rs
+++ md_converter/src/parser/flattener.rs
@@ -6,11 +6,54 @@ pub fn flatten_md_data(init_vec: Vec<MarkdownData>) -> Vec<MarkdownData> {
     let mut fin_vec = Vec::new();
     let mut vec_iter = init_vec.iter();
     let mut md_next = vec_iter.next();
-    while let Some(mut md_atom) = md_next {
+    while let Some(md_atom) = md_next {
         flatten_plaintext(md_atom, &mut md_next, &mut vec_iter, &mut fin_vec);
+        flatten_unordered_list(md_atom, &mut md_next, &mut vec_iter, &mut fin_vec);
     }
 
     fin_vec
+}
+
+fn flatten_unordered_list <'a> (
+    mut md_atom: &'a MarkdownData,
+    md_next: &mut Option<&'a MarkdownData>,
+    vec_iter: &mut std::slice::Iter<'a, MarkdownData>,
+    fin_vec: &mut Vec<MarkdownData>
+) {
+    let mut data_holder = Vec::new();
+    while let MarkdownForm::UnorderedList { .. } = md_atom.form {
+        data_holder.push(md_atom);
+
+        *md_next = vec_iter.next();
+        match *md_next {
+            Some(atom) => md_atom = atom,
+            None => break
+        };
+    }
+
+    if data_holder.len() > 0 {
+        let mut curr_md_form = data_holder.pop().expect("len > 0").clone();
+        while data_holder.len() > 0 {
+            let new_data = data_holder.pop().expect("len > 0");
+            #[allow(unused_variables)]
+            let form = if let MarkdownForm::UnorderedList {
+                indents,
+                inner_bullet
+            } = &new_data.form {
+                // Inner bullet should be None at this point anyways
+                let inner_bullet = Some(Box::new(curr_md_form));
+                let indents = *indents;
+                MarkdownForm::UnorderedList { indents, inner_bullet }
+            } else {
+                panic!("First if statement didn't get true by default");
+            };
+            curr_md_form = MarkdownData {
+                form,
+                inner_data: new_data.inner_data.clone()
+            }
+        }
+        fin_vec.push(curr_md_form);
+    }
 }
 
 fn flatten_plaintext <'a> (
@@ -73,5 +116,40 @@ mod tests {
         let data = data.get(0).expect("Data made manually");
         assert_eq!(data.inner_data, "Line 1 Still line 1".to_string());
         assert_eq!(data.form, MarkdownForm::PlainText { has_line_break: true });
+    }
+
+    #[test]
+    fn flattens_unordered_list() {
+        let data = vec![
+            MarkdownData {
+                form: MarkdownForm::UnorderedList {
+                    indents: 0,
+                    inner_bullet: None
+                },
+                inner_data: "Outer Bullet".to_string()
+            },
+            MarkdownData {
+                form: MarkdownForm::UnorderedList {
+                    indents: 1,
+                    inner_bullet: None
+                },
+                inner_data: "Inner Bullet".to_string()
+            }
+        ];
+        let data = flatten_md_data(data);
+        let data = data.get(0).expect("Data made manually").clone();
+        assert_eq!(data, MarkdownData {
+            form: MarkdownForm::UnorderedList {
+                indents: 0,
+                inner_bullet: Some(Box::new(MarkdownData {
+                    form: MarkdownForm::UnorderedList {
+                        indents: 1,
+                        inner_bullet: None
+                    },
+                    inner_data: "Inner Bullet".to_string()
+                }))
+            },
+            inner_data: "Outer Bullet".to_string()
+        });
     }
 }
