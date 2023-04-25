c092738d2b20723eb4561c82da5cd97cf03609fc
otamam818 <otamam818@gmail.com>
2023-04-25 19:53:52 +1000

    Started working on integrating typescript with animation

--- web/src/WelcomePage/RightSide.svelte
+++ web/src/WelcomePage/RightSide.svelte
@@ -1,6 +1,14 @@
+<script lang="ts">
+  let animationOrder: String = "first";
+
+  setTimeout(() => {
+    animationOrder = "second";
+  }, 2000)
+
+</script>
 
 <div class="right-side">
-  <div class="oval skinny"></div>
+  <div class={`oval skinny ${animationOrder}`}></div>
 </div>
 
 <style lang="scss">
@@ -11,7 +19,40 @@
     filter: blur(50px);
     opacity: 63%;
     position: absolute;
-    animation: skinny-oval-beat 2s infinite;
+
+    &.first {
+      animation: get-fat-inside 2s forwards;
+    }
+
+    &.second {
+      animation: skinny-oval-beat 2s infinite;
+    }
+  }
+
+  @keyframes get-fat-inside {
+    // intro to the oval
+    0% {
+      opacity: 0;
+      left: 30%;
+      top: 30%;
+      width: 90vw;
+      height: 80vh;
+    }
+
+    50% {
+      left: 60%;
+      top: 15%;
+      width: 60vh;
+      height: 80vh;
+    }
+
+    100% {
+      opacity: 1;
+      left: 60%;
+      top: 30%;
+      width: 30vh;
+      height: 40vh;
+    }
   }
 
   @keyframes skinny-oval-beat {
